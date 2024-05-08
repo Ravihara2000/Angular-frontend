3144ea27f18f58dec9e6b4ab1f3dd8ecb958e4ad
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2024-05-08 09:03:54 +0530

    ngFor implemented

--- techApp/src/app/books/books.component.ts
+++ techApp/src/app/books/books.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-
+interface Book{
+  name:string
+  author:string
+  image:string
+}
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
@@ -7,8 +11,8 @@ import { Component } from '@angular/core';
 })
 export class BooksComponent {
 
-  name="Madol duwa";
-  author="Martin wick";
+  // name="Madol duwa";
+  // author="Martin wick";
 
   isDisabled : boolean=false;
 
@@ -31,5 +35,23 @@ export class BooksComponent {
   toggleBooks(){
     this.isShowing=!this.isShowing;
   }
+
+  books:Book[]=[
+    {
+      name:"Madol duwa",
+      author:"Martin wick",
+      image:"https://upload.wikimedia.org/wikipedia/en/5/5c/MadolDoova.jpg"
+    },
+    {
+      name:"Madol duwa2",
+      author:"Martin wick2",
+      image:"https://upload.wikimedia.org/wikipedia/en/5/5c/MadolDoova.jpg"
+    },
+    {
+      name:"Madol duwa3",
+      author:"Martin wick3",
+      image:"https://upload.wikimedia.org/wikipedia/en/5/5c/MadolDoova.jpg"
+    },
+  ];
 
 }
